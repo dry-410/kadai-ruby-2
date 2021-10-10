2a5e9417b1f04f8eabe13286f5900cd4aaaca594
dry-410 <kk-2098m4@outlook.jp>
2021-10-10 04:21:06 +0000

    second commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,17 @@
+def fizzbuzz(num)
+  if num % 15 == 0
+   'FizzBuzz'
+  elsif num % 3 == 0
+   'Fizz'
+  elsif num % 5 == 0
+   'Buzz'
+  else 
+   num.to_s
+  end
+end
+
 num_max = 100
 (1..num_max).each do |num|
+puts fizzbuzz(num)
+                   end
 
-def fizzbuzz(num)
-    if num % 15 == 0
-        puts 'FizzBuzz'
-    elsif num % 3 == 0
-        puts 'Fizz'
-    elsif num % 5 == 0
-        puts 'Buzz'
-    else 
-        puts num
-    end
-end
-    fizzbuzz(num)
-end
